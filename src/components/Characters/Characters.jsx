2f5068bb11d9de5180396a99ccc658368861d354
Ovinson Lugo <ovinsonlugo12@gmail.com>
2025-08-16 13:40:58 -0600

    Change layout on the Card children and delete commented code in Characters.jsx. Delete unused styles and add some to improve site

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -1,88 +1,3 @@
-// import { useState } from "react";
-
-// import Card from "../Card/Card";
-// import CardDetailsModal from "../CardDetailsModal/CardDetailsModal";
-// import CharacterModalContent from "../CharacterModalContent/CharacterModalContent";
-
-// import "./Characters.css";
-
-// function Characters({ characters }) {
-//   const [selectedCard, setSelectedCard] = useState(null);
-//   console.log(characters);
-//   const getCardStyle = (character) => {
-//     const imgSrc = character?.images?.[0] || character?.images?.[1];
-
-//     return imgSrc
-//       ? {
-//           backgroundImage: `url(${imgSrc})`,
-//           backgroundSize: "cover",
-//           backgroundPosition: "center",
-//           backgroundRepeat: "no-repeat",
-//         }
-//       : {};
-//   };
-
-//   const displayCharacters = () => {
-//     return characters.map((character) => (
-//       <Card
-//         className="character__item"
-//         key={character.id}
-//         style={getCardStyle(character)}
-//         onClick={() => setSelectedCard(character)}
-//       >
-//         <div className="character__content">
-//           <h2 className="character__title">{character.name}</h2>
-//           {character.jutsu && (
-//             <div>
-//               <h3 className="character__subtitle">Jutsus</h3>
-//               <p className="character__info">{character.jutsu.join(", ")}</p>
-//             </div>
-//           )}
-//           {character.natureType && (
-//             <div>
-//               <h3 className="character__subtitle">Nature Type</h3>
-//               <p className="character__info">
-//                 {character.natureType.join(", ")}
-//               </p>
-//             </div>
-//           )}
-//           {character.family && (
-//             <div>
-//               <h3 className="character__subtitle">Family</h3>
-//               <div className="character__info">
-//                 {Object.entries(character.family).map(([k, v]) => (
-//                   <p className="character__key" key={v}>
-//                     {`${k}: `}
-//                     <span className="character__value">{v}</span>
-//                   </p>
-//                 ))}
-//               </div>
-//             </div>
-//           )}
-//         </div>
-//       </Card>
-//     ));
-//   };
-
-//   return (
-//     <>
-//       <div>
-//         <h1 className="characters__title">Characters</h1>
-//         <div className="characters">
-//           <ul className="character__list">{displayCharacters()}</ul>
-//         </div>
-//       </div>
-//       <CardDetailsModal
-//         isOpen={!!selectedCard}
-//         onClose={() => setSelectedCard(null)}
-//         title={selectedCard?.name}
-//       >
-//         {selectedCard && <CharacterModalContent card={selectedCard} />}
-//       </CardDetailsModal>
-//     </>
-//   );
-// }
-
 // export default Characters;
 // src/components/Characters/Characters.jsx
 import { useEffect, useState } from "react";
@@ -180,6 +95,7 @@ function Characters() {
           {!loading && !err && items.length > 0 && (
             <>
               <ul className="character__list">
+                {console.log(items)}
                 {items.map((character) => (
                   <Card
                     className="character__item"
@@ -190,9 +106,30 @@ function Characters() {
                     <div className="character__content">
                       <h2 className="character__title">{character.name}</h2>
                       <div className="character__subtitles">
-                        {character.debut && (
-                          <span>{Object.entries(character.debut)}</span>
-                        )}
+                        {Object.entries(character)
+                          .filter(
+                            ([key]) =>
+                              key !== "id" &&
+                              key !== "images" &&
+                              key !== "name" &&
+                              key !== "voiceActors"
+                          )
+                          .map(
+                            ([key]) =>
+                              key && (
+                                <div
+                                  key={key}
+                                  className="character__subtitle-container"
+                                >
+                                  <span
+                                    className="character__subtitle"
+                                    key={key}
+                                  >
+                                    {key}
+                                  </span>
+                                </div>
+                              )
+                          )}
                       </div>
                     </div>
                   </Card>
