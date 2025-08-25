77ce2d6efb620d1a6bf64b60c935a2237810c24b
Ovinson Lugo <ovinsonlugo12@gmail.com>
2025-08-25 15:55:30 -0600

    Add slash

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/shinobiarchives",
+  base: "/shinobiarchives/",
   server: {
     port: 3000,
     open: true,
